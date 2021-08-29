9b1179040a3463c36d36a65331c61904d78956c2
AlberVini <viiniciusalberto@gmail.com>
2021-08-28 22:51:49 -0300

    create task page

--- django1/tasks/models.py
+++ django1/tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    OPTIONS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+
+    title = models.CharField(max_length=255, default='No title')
+    description = models.TextField(default='No description')
+    status = models.CharField(
+        max_length=5,
+        choices=OPTIONS,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- django1/tasks/views.py
+++ django1/tasks/views.py
@@ -1,12 +1,19 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Task
 
-def helloWorld(request):
-    return HttpResponse('Hello World!')
 
 def taskList(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'task':tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
 
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
+
+def helloWorld(request):
+    return HttpResponse('Hello World!')
