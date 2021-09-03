6842b294ed1ad3850b9f0d77f4bc98ed9069e683
AlberVini <viiniciusalberto@gmail.com>
2021-09-03 00:24:44 -0300

    auth feature

--- django1/tasks/views.py
+++ django1/tasks/views.py
@@ -1,11 +1,15 @@
+from django1.settings import LOGIN_REDIRECT_URL
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import Task, TaskForm
 from django.contrib import messages
 
+
+@login_required
 def taskList(request):
 
     search = request.GET.get('search')
@@ -22,10 +26,12 @@ def taskList(request):
 
     return render(request, 'tasks/list.html', {'tasks':tasks})
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -41,6 +47,7 @@ def newTask(request):
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form' : form})
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -57,6 +64,7 @@ def editTask(request, id):
     else:
         return render(request, 'tasks/edittask.html', {'form':form, 'task':task})
 
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
